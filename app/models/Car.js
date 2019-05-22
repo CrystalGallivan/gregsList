6dcce492ee857c853630ae975efb690a0a022024
Your Name <crystalgallivan@gmail.com>
2019-05-22 10:51:39 -0600

    template

--- app/models/Car.js
+++ app/models/Car.js
@@ -6,9 +6,20 @@ export default class Car {
     this.imgUrl = data.imgUrl
     this.year = data.year
     this.price = data.price
-    this.description = data.description
+    this.description = data.description || 'No Description Provided'
   }
   get Template() {
-    return ``
+    return ` 
+    <div class="col-3" id="">
+    <div class="card">
+    <img class="card-img-top" src="${this.imgUrl}" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">${this.make} - ${this.model} - ${this.year}</h5>
+    <h5 class="card-title">$${this.price}</h5>
+    <p class="card-text">${this.description}</p>
+    <button class="btn btn-primary">Bid</button>
+    </div>
+    </div>
+    </div>`
   }
 }
